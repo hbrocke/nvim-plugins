16ed640b7c1e8f9d1ccbb0b027b7da37150e4434
Hendrik Brocke <hendrikbrocke@mailbox.org>
2024-10-06 18:20:08 +0200

    Add basic plugins

---
+++ lua/plugins/test.lua
@@ -0,0 +1,38 @@
+
+return {
+  {
+    'navarasu/onedark.nvim',
+    lazy = false,
+    name = "onedark",
+    priority = 1000,
+  },
+  {
+    'tanvirtin/monokai.nvim',
+    lazy = false,
+    name = "monokai",
+    priority = 1000,
+  },
+{
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+},
+{
+  "Tsuzat/NeoSolarized.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+},
+  {
+'Abstract-IDE/Abstract-cs',
+
+    lazy = false,
+    priority = 1000,
+},
+  { "EdenEast/nightfox.nvim",
+  
+    lazy = false,
+    priority = 1000,
+  }
+}
+
+
