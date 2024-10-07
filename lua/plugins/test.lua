456dd003eaae0e1933371adac08d2d3a2ac290c2
Hendrik Brocke <hendrikbrocke@mailbox.org>
2024-10-07 22:39:05 +0200

    Fix formatting of files

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -12,24 +12,27 @@ return {
     name = "monokai",
     priority = 1000,
   },
-{
+  {
     "scottmckendry/cyberdream.nvim",
     lazy = false,
     priority = 1000,
-},
-{
-  "Tsuzat/NeoSolarized.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-},
+  },
   {
-'Abstract-IDE/Abstract-cs',
-
+    "Tsuzat/NeoSolarized.nvim",
     lazy = false,
     priority = 1000,
-},
+  },
+  {
+    "Abstract-IDE/Abstract-cs",
+    lazy = false,
+    priority = 1000,
+  },
   { "EdenEast/nightfox.nvim",
-  
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "sainnhe/sonokai",
     lazy = false,
     priority = 1000,
   }
